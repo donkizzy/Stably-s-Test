7d0cc5404f680647b66d03e4f176305579c6c195
Amos Godwin <Amosgodwin500@gmail.com>
2021-05-24 00:44:01 +0100

    imported  the algorithm and added a route to find the highest prime nnumber less than a number

--- Index.ts
+++ Index.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express, { json, NextFunction, Request, Response } from "express";
 import path from 'path';
+import highestPrimeNumber from "./src/utils/HighestPrimeNumber";
 
 // Initialize express app
 const app = express();
@@ -12,9 +13,12 @@ app.use(json());
 app.get("/", (req: Request, res: Response): void => {
     //TODO : Serve html page here
 });
+app.post('/find/:num',(req: Request, res: Response): void => {
+    var primeNumber = highestPrimeNumber(Number(req.params.num)) ;
+    res.send({primeNumber});
+    });
 
 app.use("*", (req: Request, res: Response): void => {
-    ``
     res.status(404).send('please check your route');
 });
 

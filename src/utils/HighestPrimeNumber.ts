fca61f8e24a1134e5f086e44a8b451cc65b5e36b
Amos Godwin <Amosgodwin500@gmail.com>
2021-05-24 00:40:27 +0100

    added algorithm to find highest prime number less than a particular number

---
+++ src/utils/HighestPrimeNumber.ts
@@ -0,0 +1,21 @@
+
+
+const highestPrimeNumber = (num: number): number => {
+    for (let i = num - 1; i >= 0; i--) {
+      if (isPrime(i)) {
+        return i;
+      }
+    }
+    return 0;
+  };
+  
+  const isPrime = (num: number): boolean => {
+    if (num < 2) return false;
+    const numsQrt = Math.sqrt(num);
+    for (let i = 2; i <= numsQrt; i++) {
+      if (num % i === 0) return false;
+    }
+    return num > 1;
+  };
+  
+  export default  highestPrimeNumber ;

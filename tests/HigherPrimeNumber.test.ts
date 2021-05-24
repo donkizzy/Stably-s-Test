0cbf7487ce347740af6f7081fbf2800e3ed4eed2
Amos Godwin <Amosgodwin500@gmail.com>
2021-05-24 23:36:26 +0100

    wrot tests to test for highest prime numbers lesser than a number

--- tests/HigherPrimeNumber.test.ts
+++ tests/HigherPrimeNumber.test.ts
@@ -1,8 +1,17 @@
-import { isPrime } from '../src/utils/HighestPrimeNumber';
+import { highestPrimeNumber,isPrime } from '../src/utils/HighestPrimeNumber';
 
 test('Test for prime number',() => {
     expect(isPrime(7)).toBeTruthy();
     expect(isPrime(0)).toBeFalsy();
     expect(isPrime(1)).toBeFalsy();
+    expect(isPrime(-1)).toBeFalsy();
     expect(isPrime(2)).toBeTruthy();
+});
+
+test('Tests for Highest Prime Number lesser than a number',() => {
+    expect(highestPrimeNumber(55)).toBe(53);
+    expect(highestPrimeNumber(1)).toBe(0);
+    expect(highestPrimeNumber(0)).toBe(0);
+    expect(highestPrimeNumber(-5)).toBe(0);
+    expect(highestPrimeNumber(7)).toBe(5);
 });

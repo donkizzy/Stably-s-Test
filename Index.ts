beb3d5306dd0b11b710e98c201957e2c80f9c173
Amos Godwin <Amosgodwin500@gmail.com>
2021-05-24 15:58:10 +0100

    - imported the html into the express app - served the page on the default app route

--- Index.ts
+++ Index.ts
@@ -9,14 +9,15 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(json());
+app.use(express.static(path.join(__dirname, '../src/public')));
 
 app.get("/", (req: Request, res: Response): void => {
-    //TODO : Serve html page here
+    res.sendFile(path.join(__dirname, '../src/views', 'index.html'));
 });
-app.post('/find/:num',(req: Request, res: Response): void => {
-    var primeNumber = highestPrimeNumber(Number(req.params.num)) ;
-    res.send({primeNumber});
-    });
+app.post('/find/:num', (req: Request, res: Response): void => {
+    var primeNumber = highestPrimeNumber(Number(req.params.num));
+    res.send({ primeNumber });
+});
 
 app.use("*", (req: Request, res: Response): void => {
     res.status(404).send('please check your route');

83d5abde9b414850c7028f869f1042dd7d556a5a
Amos Godwin <Amosgodwin500@gmail.com>
2021-06-02 22:15:34 +0100

    split my server and routes because of my tests Used copyfiles to copy the files to the build folder

--- Index.ts
+++ Index.ts
@@ -1,29 +1,7 @@
-import cors from "cors";
-import express, { json, Request, Response } from "express";
-import path from 'path';
-import { highestPrimeNumber } from "./src/utils/HighestPrimeNumber";
+import app from './src/App';
 
-// Initialize express app
-const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(json());
-app.use(express.static(path.join(__dirname, '../src/public')));
-
-app.get("/", (req: Request, res: Response): void => {
-    res.sendFile(path.join(__dirname, '../src/views', 'index.html'));
+app.listen(PORT, () => {
 });
-app.post('/find', (req: Request, res: Response): void => {
-    var primeNumber = highestPrimeNumber(Number(req.body.num));
-    res.send({ primeNumber });
-});
-
-app.use("*", (req: Request, res: Response): void => {
-    res.status(404).send('please check your route');
-});
-
-app.listen(PORT, async (): Promise<void> =>{
-    
-}
-)
+export default app;
---
+++ src/App.ts
@@ -0,0 +1,24 @@
+import cors from "cors";
+import express, { json, Request, Response } from "express";
+import path from 'path';
+import { highestPrimeNumber } from "./utils/HighestPrimeNumber";
+
+// Initialize express app
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(express.static(path.join(__dirname, '../src/public')));
+app.get("/", (req: Request, res: Response): void => {
+    res.status(200).sendFile(path.join(__dirname, '../src/views', 'index.html'));
+});
+app.post('/', (req: Request, res: Response): void => {
+    var primeNumber = highestPrimeNumber(Number(req.body.num));
+    res.send({ primeNumber });
+});
+
+app.use("*", (req: Request, res: Response): void => {
+    res.status(404).send('please check your route');
+});
+
+export default app;

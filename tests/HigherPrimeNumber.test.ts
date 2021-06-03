fa6e51721dd63b2818feef69284ed19bf94d1d95
Amos Godwin <Amosgodwin500@gmail.com>
2021-06-03 12:06:34 +0100

    updated my tests for highest prime number

--- tests/HigherPrimeNumber.test.ts
+++ tests/HigherPrimeNumber.test.ts
@@ -1,43 +1,43 @@
-import { highestPrimeNumber,isPrime } from '../src/utils/HighestPrimeNumber';
+import { highestPrimeNumber, isPrime } from '../src/utils/HighestPrimeNumber';
 
-describe('Test for prime number',() =>{
-    test('Test for whole numbers',() => {
+describe('Test for prime number', () => {
+    test('Test for whole numbers', () => {
         expect(isPrime(7)).toBeTruthy();
-       
+
     });
-    test('Test for zero',() => {
+    test('Test for zero', () => {
         expect(isPrime(0)).toBeFalsy();
     });
-    test('Test for 1',() => {
+    test('Test for 1', () => {
         expect(isPrime(1)).toBeFalsy();
     });
-    test('Test for numbers less than 0',() => {
+    test('Test for numbers less than 0', () => {
         expect(isPrime(-1)).toBeFalsy();
     });
-    test('Test for 2 as the first prime number',() => {
+    test('Test for 2 as the first prime number', () => {
         expect(isPrime(2)).toBeTruthy();
     });
 });
 
-describe('Tests for Highest Prime Number lesser than a number',() =>{
+describe('Tests for Highest Prime Number lesser than a number', () => {
 
-    test('Tests for Highest Prime Number as a whole number',() => {
+    test('Tests for Highest Prime Number as a whole number', () => {
         expect(highestPrimeNumber(55)).toBe(53);
     });
-    test('Tests for Highest Prime Number for 1',() => {
+    test('Tests for Highest Prime Number for 1', () => {
         expect(highestPrimeNumber(1)).toBe(0);
     });
-    test('Tests for Highest Prime Number for 0',() => {
+    test('Tests for Highest Prime Number for 0', () => {
         expect(highestPrimeNumber(0)).toBe(0);
     });
-    test('Tests for Highest Prime Number for numbers less than 0',() => {
+    test('Tests for Highest Prime Number for numbers less than 0', () => {
         expect(highestPrimeNumber(-5)).toBe(0);
     });
-    test('Tests for Highest Prime Number lesser than a number',() => {
-        expect(highestPrimeNumber(5003)).toBe(4999);        
+    test('Tests for Highest Prime Number lesser than a number', () => {
+        expect(highestPrimeNumber(5003)).toBe(4999);
     });
-    test('Tests for Highest Prime Number as a large whole number',() => {
+    test('Tests for Highest Prime Number as a large whole number', () => {
         expect(highestPrimeNumber(273735)).toBe(273727);
-        
+
     });
 });
